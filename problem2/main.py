7d4b82e0b369ca72d38bf9a47766d8a951cd7f71
alisofyansaputra <Buffware4@gmail.com>
2024-03-03 17:23:34 +0700

    problem2.main.py

--- problem2/main.py
+++ problem2/main.py
@@ -1,4 +1,18 @@
 def primeX(x):
+    primes = []
+    num = 2 
+    
+    while len(primes) < x:
+        is_prime = True
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0 :
+                is_prime = False 
+                break
+        if is_prime:
+            primes.append(num)
+        num += 1
+    return primes[-1]
+
     return 0
 
 if __name__ == "__main__":
