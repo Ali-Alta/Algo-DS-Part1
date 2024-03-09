8b7f17844f432ac26b4fa4083c81bbe74e212ee1
alisofyansaputra <Buffware4@gmail.com>
2024-03-09 20:03:29 +0700

    problem3.main.py

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,13 @@
 def fibonacci(number):
-    return 0
-
+    if number <= 0:
+        return 0
+    elif number == 1 :
+        return 1
+    else:
+        a, b =0, 1
+        for _ in range(number - 1):
+            a, b = b, a + b
+    return b
 if __name__ == "__main__":
     print(fibonacci(0))  # 0
     print(fibonacci(2))  # 1
