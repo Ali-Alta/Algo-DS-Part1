16defd48c7a2d665da06d7d6b1741de2db306671
alisofyansaputra <Buffware4@gmail.com>
2024-03-09 20:17:54 +0700

    problem4.main.py

--- problem4/main.py
+++ problem4/main.py
@@ -1,6 +1,25 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def generate_primes_grid(width, height, start):
     result = ""
-    return result
+    current_num = start
+    
+
+    for  _ in range(height):
+        row = []
+        for  _ in range(width):
+            while not is_prime(current_num):
+                current_num += 1
+            row.append(current_num)
+            current_num += 1
+        result += " ".join(map(str, row)) + "\n"
+    return result.strip()    
 
 if __name__ == "__main__":
     print(generate_primes_grid(2, 3, 13))
