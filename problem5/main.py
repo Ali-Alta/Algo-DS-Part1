a4b54ad6aded5e08f826ba88ffaaff6e0438ed91
alisofyansaputra <Buffware4@gmail.com>
2024-03-09 20:29:16 +0700

    problem5.main.py

--- problem5/main.py
+++ problem5/main.py
@@ -1,5 +1,13 @@
 def max_sequence(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+    
+    for num in arr:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
     return 0
+
 
 if __name__ == "__main__":
     print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))  # 6
